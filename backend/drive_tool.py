e086d632454d86de27ad4f140eec845d7e5f637b
Sheetal Patel <sheetalpatel8603@gmail.com>
2026-05-16 14:02:41 +0530

    Using environment credentials

--- backend/drive_tool.py
+++ backend/drive_tool.py
@@ -1,12 +1,16 @@
+import os
+import json
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
-SERVICE_ACCOUNT_FILE = 'credentials/service_account.json'
+service_account_info = json.loads(
+    os.getenv("SERVICE_ACCOUNT_JSON")
+)
 
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE,
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info,
     scopes=SCOPES
 )
 
